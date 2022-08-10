800a165c622d98efacdf1bd2bec701219e3b58fe
Lydia Jagdeo <99758270+lydiajagdeo@users.noreply.github.com>
2022-08-10 19:39:59 -0400

    Updated Methods exercise
    Completed exercise 2 and added it to repo

--- Program.cs
+++ Program.cs
@@ -17,6 +17,39 @@ namespace Methods
 
             Console.WriteLine($"Cool cool and what is your favorite band?");
             var band = Console.ReadLine();
+            
+            
+            var addition = Sum(2, 6);
+            var multiplication = Multiply(2, 4, 6);
+            var subtraction = Minus(9, 4);
+            var division = Divide(60, 3);
+
+            static int Sum(int num1, int num2)
+            {
+                var answer = num1 + num2;
+                Console.WriteLine(answer);
+                return answer;
+            }
+            static int Multiply(int num1, int num2, int num3)
+            {
+                var answer2 = num1 * num2 * num3;
+                Console.WriteLine(answer2);
+                return answer2;
+            }
+
+            static int Divide(int num1, int num2)
+            {
+                var answer3 = num1 / num2;
+                Console.WriteLine(answer3);
+                return answer3;
+            }
+
+            static int Minus(int num1, int num2)
+            {
+                var answer4 = num1 - num2;
+                Console.WriteLine(answer4);
+                return answer4;
+            }
         }
     }
 }
